080190bdc1a34264db869dcf9ba5ef789ef31312
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-03-07 17:58:54 +0700

    membuat prosedur bubble sort

--- ASD_Pert_Ketiga_234.cpp
+++ ASD_Pert_Ketiga_234.cpp
@@ -28,6 +28,21 @@ void input() {
     }
 }
 
+void bubbleSortArray() {
+    int pass = 1;
+
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        pass = pass + 1;
+    } while (pass <= n);
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
